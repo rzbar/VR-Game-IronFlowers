19b7336322963cdca43feef0ae0f5d05fb74638e
“rzbar” <202205501711@smail.xtu.edu.cn>
2024-08-24 18:42:23 +0800

    test

--- Assets/Scripts/IronFlower/EffectBron.cs
+++ Assets/Scripts/IronFlower/EffectBron.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.Content.Interaction;
+[RequireComponent(typeof(IronFlowerEffect))]
 public class EffectBron : MonoBehaviour
 {
-    private IronFlowerEffect ironEffect;
+    public IronFlowerEffect ironEffect;
     public float modifyNum;
     private void Awake()
     {
         ironEffect = GetComponent<IronFlowerEffect>();
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Stick" /*&& other.gameObject.GetComponent<Rigidbody>().velocity.magnitude > 1*/)
+        {
+            Quaternion rotation = Quaternion.LookRotation((other.transform.position - transform.position).normalized);
+            //float speed = modifyNum * other.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+            ironEffect.IronFlowerEffectGenerator(transform.position, rotation, 5, 0.8f);
+        }
+        else
+        {
+            print("too slow");
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.tag== "Stick"&& collision.rigidbody.velocity.magnitude>1)
+        if (collision.gameObject.tag == "Stick" /*&& other.gameObject.GetComponent<Rigidbody>().velocity.magnitude > 1*/)
         {
             Quaternion rotation = Quaternion.LookRotation((collision.transform.position - transform.position).normalized);
-            float speed = modifyNum * collision.rigidbody.velocity.magnitude;
-            ironEffect.IronFlowerEffectGenerator(transform.position, rotation, speed,0.8f);
+            //float speed = modifyNum * other.gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+            ironEffect.IronFlowerEffectGenerator(transform.position, rotation, 5, 0.8f);
+        }
+        else
+        {
+            print("too slow");
         }
     }
 }
