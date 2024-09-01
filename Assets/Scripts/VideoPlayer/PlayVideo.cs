7c562438c67ffbbd97be13510590031d433bd1af
“rzbar” <202205501711@smail.xtu.edu.cn>
2024-09-01 19:38:19 +0800

    视频播放相关功能初版

--- Assets/Scripts/VideoPlayer/PlayVideo.cs
+++ Assets/Scripts/VideoPlayer/PlayVideo.cs
@@ -1,44 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class PlayVideo : MonoBehaviour
 {
-    public GameObject StartMove;//启动动画
-    private void Awake()
+    public VideoPlayer videoPlayer;
+    public Button beginButton;
+    public Slider videoSlider;
+    //public AudioSource audioSource;
+    public TextMeshProUGUI totalTimeText;      // 用于显示总时长
+    public TextMeshProUGUI currentTimeText;    // 用于显示当前时间
+    public Toggle videoMute;
+    public VideoClip[] videoClips;
+
+    private float videoLength; // 视频总时长（秒）  
+
+    void Start()
     {
-        StartPlay();
+        beginButton.onClick.AddListener(OnClickPlay);
+        // 获取视频总时长  
+        videoLength = (float)videoPlayer.length;
+        // 设置总时长文本  
+        totalTimeText.text = FormatTime(videoLength);
+
+        // 监听视频播放事件以更新UI  
+        videoPlayer.prepareCompleted += OnVideoReady;
+        videoPlayer.frameReady += OnFrameReady;
+
+        // 设置Slider监听视频时间  
+        videoSlider.onValueChanged.AddListener(OnSliderValueChanged);
+        videoMute.onValueChanged.AddListener(OnMute);
+        //OnMute(false);
+        
     }
 
-    public void StartPlay()
+    private void OnClickPlay()
     {
-        if (StartMove != null)//播放启动动画
+        if (videoPlayer.isPlaying)
         {
-            VideoPlayer videoPlayer = StartMove.GetComponent<VideoPlayer>();
-            StartMove.SetActive(true);//默认对象关闭
-            //videoPlayer.frame = 100;//跳过前100帧
-            videoPlayer.loopPointReached += VideoPlayer_loopPointReached;//添加播放结束事件
-            StartMove.transform.SetAsLastSibling();
-            videoPlayer.Play();//播放视频
+            PauseVideo();
         }
         else
-            Debug.LogError("启动动画 StartMove=null");
+        {
+            OnPlayVideo();
+        }
     }
 
-    /// <summary>
-    /// 播放结束或播放到循环的点时被执行
-    /// </summary>
-    private void VideoPlayer_loopPointReached(VideoPlayer source)
+    private void OnMute(bool value)
     {
-        if (StartMove != null)
-        {
-            StartMove.SetActive(false);
-            //GameObject.Destroy(StartMove);
-            //StartMove = null;
-        }
-        else
-            Debug.LogError("启动动画 播放结束 StartMove=null");
+        videoPlayer.SetDirectAudioMute(0, value);
     }
+
+    private void OnVideoReady(VideoPlayer source)
+    {
+        // 视频准备完成后可以开始播放  
+        videoPlayer.Play();
+    }
+
+    private void OnFrameReady(VideoPlayer source, long frameIdx)
+    {
+        // 更新当前时间文本  
+        currentTimeText.text = FormatTime((float)source.time);
+
+        // 更新Slider的Value以匹配视频的当前帧  
+        videoSlider.value = (int)(source.frame * videoSlider.maxValue / videoPlayer.frameCount);
+    }
+
+    private void OnSliderValueChanged(float value)
+    {
+        // 当Slider的值改变时，设置视频到相应的帧  
+        videoPlayer.frame = (long)(value * videoPlayer.frameCount / videoSlider.maxValue);
+    }
+
+    // 辅助方法用于格式化时间  
+    public static string FormatTime(float seconds)
+    {
+        int minutes = (int)Mathf.Floor(seconds / 60);
+        int remainingSeconds = (int)Mathf.Round(seconds % 60);
+        string formatted = minutes.ToString("D2") + ":" + remainingSeconds.ToString("D2");
+        return formatted;
+    }
+
+    // 其他方法，如播放、暂停等可以在此处添加  
+    public void OnPlayVideo() { videoPlayer.Play(); }
+    public void PauseVideo() { if (videoPlayer.isPlaying) videoPlayer.Pause(); } 
 
 }
